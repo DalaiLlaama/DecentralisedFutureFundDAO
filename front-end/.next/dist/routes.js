03146d07b957e9236217df9b51e929e0271523bf
DalaiLlaama <glamperd@gmail.com>
2018-09-10 16:08:05 +1000

    ignore

--- front-end/.next/dist/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/members/new', '/members/new').add('/members/:address', '/members/show').add('/proposals/new', '/proposals/new').add('/proposals/:address', '/proposals/show');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNFLEFBREYsSUFDTSxBQUROLGdCQUNzQixBQUR0QixnQkFFRSxBQUZGLElBRU0sQUFGTixxQkFFMkIsQUFGM0IsaUJBR0UsQUFIRixJQUdNLEFBSE4sa0JBR3dCLEFBSHhCLGtCQUlFLEFBSkYsSUFJTSxBQUpOLHVCQUk2QixBQUo3Qjs7QUFNQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6IkM6L2Rldi93b3Jrc3BhY2UvRGVjZW50cmFsaXNlZEZ1dHVyZUZ1bmREQU8vZnJvbnQtZW5kIn0=
