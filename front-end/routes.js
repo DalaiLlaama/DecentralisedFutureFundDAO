47154ec81c93171943c1ca86e114a4fe1e757070
DalaiLlaama <glamperd@gmail.com>
2018-09-16 19:42:37 +1000

    Rearrange repo - move front-end to seaparate repo

--- front-end/routes.js
+++
@@ -1,8 +0,0 @@
-const routes = require('next-routes')();
-
-routes
-	.add('/members/new', '/members/new')
-	.add('/members/:address', '/members/show')
-	.add('/proposals/new', '/proposals/new')
-
-module.exports = routes;
