83daa0d2f17a16fd797297da09725a520e084050
DalaiLlaama <glamperd@gmail.com>
2018-09-16 19:27:38 +1000

    move certs

--- front-end/httpsserver.js
+++ front-end/httpsserver.js
@@ -3,11 +3,10 @@ const next = require('next');
 const crypto = require('crypto');
 const fs = require('fs');
 
-var privateKey = fs.readFileSync('./localhost.key').toString();
-var certificate = fs.readFileSync('./localhost.crt').toString();
+var privateKey = fs.readFileSync('./certs/localhost.key').toString();
+var certificate = fs.readFileSync('./certs/localhost.crt').toString();
 
 var credentials = tls.createSecureContext({key: privateKey, cert: certificate, secureProtocol: 'TLSv1_2_method' });
-//var credentials = tls.createSecureContext({key: privateKey, cert: certificate});
 
 const app = next({
 	dev: process.env.NODE_ENV !== 'production'
